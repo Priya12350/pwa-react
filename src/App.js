2fa12c5dfe7b39cea86671c83862f72c808ce9b1
Priya12350 <1823024@saec.ac.in>
2021-07-10 13:11:43 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
-  return (
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(10);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    fetchProducts();
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+		if (
+			Math.ceil(window.innerHeight + document.documentElement.scrollTop) !== document.documentElement.offsetHeight ||
+			isFetching
+		)
+			return;
+		setIsFetching(true);
+		console.log(isFetching);
+	};
+
+
+  
+  const fetchProducts = async () => {
+    setTimeout(async () => {
+      const { data } = await Axios.get(
+      "https://type.fit/api/quotes?page=${page}");
+			setPage(page + 1);
+      const products = data;
+      setProducts(products);
+      console.log(products);
+    }, 1000);
+  };
+
+  useEffect(() => {
+		if (!isFetching) return;
+		fetchMoreListItems();
+	}, [isFetching]);
+
+	const fetchMoreListItems = () => {
+		fetchProducts();
+		setIsFetching(false);
+	};
+
+     function loadMore() {
+      setPage((page) => page + 10);
+     }
+     
+     return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="load-more">
+      {products.map((products) => (
+        <p>{products.text}</p>
+        ))}
+       <scroll />
+       </div>
     </div>
+    {isFetching && <h1>Fetching more ...</h1>}
+    </>
   );
-}
+};
 
 export default App;
+
+
+
